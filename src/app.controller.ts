3d534388764164286173e72c3b67d32e7804ed1c
Marat Alembikov <alembikovm@gmail.com>
2024-01-22 21:26:50 +0300

    Added upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import path from 'path';
+import fs from 'fs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  uploadFile(@UploadedFile() file, @Body('username') username) {
+    const imagePath = path.join(__dirname, 'public', 'images', 'users', username, file.originalname);
+
+    fs.mkdirSync(path.join(__dirname, 'public', 'images', 'users', username), { recursive: true });
+
+    fs.writeFileSync(imagePath, file.buffer);
+
+    return 'File uploaded!';
   }
 }
