10f67ea04687c565d8aa713c77fdae0d101b6871
Marat Alembikov <alembikovm@gmail.com>
2024-01-23 22:29:32 +0300

    Added upload module

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -6,10 +6,15 @@ import * as fs from 'fs';
 export class FileUploadService {
   async uploadFile(file) {
     try {
+      if (!file.buffer || !file.originalname) {
+        throw new Error('Invalid file data');
+      }
+
       const { originalname } = file;
-      fs.writeFileSync(`${originalname}`, file.buffer);
+      fs.writeFileSync(`uploads/${originalname}`, file.buffer);
       return { message: 'File uploaded successfully' };
     } catch (error) {
+      console.error('Error uploading file:', error);
       throw new Error('Error uploading file');
     }
   }
