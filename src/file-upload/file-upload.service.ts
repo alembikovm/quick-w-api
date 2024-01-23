96561b8354beb15aafc23229c19990bf40be03f9
Marat Alembikov <alembikovm@gmail.com>
2024-01-23 23:33:47 +0300

    Added upload module

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class FileUploadService {
@@ -11,7 +12,22 @@ export class FileUploadService {
       }
 
       const { originalname } = file;
-      fs.writeFileSync(`uploads/${originalname}`, file.buffer);
+
+      // Define the upload directory based on the deployment settings
+      const uploadDir = process.env.NODE_ENV === 'production' ? 'uploads' : '../uploads';
+      
+      // Create the full path for the upload directory
+      const fullPath = path.join(__dirname, '..', 'dist', uploadDir);
+      
+      // Ensure the directory exists, if not, create it
+      if (!fs.existsSync(fullPath)) {
+        fs.mkdirSync(fullPath, { recursive: true });
+      }
+
+      // Save the file to the upload directory
+      const filePath = path.join(fullPath, originalname);
+      fs.writeFileSync(filePath, file.buffer);
+
       return { message: 'File uploaded successfully' };
     } catch (error) {
       console.error('Error uploading file:', error);
