055dfcbfc7f494dbb9ce4841beb94d3e5eed525c
Marat Alembikov <alembikovm@gmail.com>
2024-01-24 20:32:08 +0300

    upload fix

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -6,32 +6,34 @@ import * as path from 'path';
 @Injectable()
 export class FileUploadService {
   async uploadFile(file) {
+    if (!file.buffer || !file.originalname) {
+      throw new Error('Invalid file data');
+    }
+
+    const { originalname } = file;
+
+
+    // Create the full path for the upload directory
+    const fullPath = path.join(__dirname, '..', 'dist', 'uploads');
+
+    // Ensure the directory exists, if not, create it
     try {
-      if (!file.buffer || !file.originalname) {
-        throw new Error('Invalid file data');
-      }
-
-      const { originalname } = file;
-
-      // Define the upload directory based on the deployment settings
-      const uploadDir = process.env.NODE_ENV === 'production' ? 'uploads' : '../uploads';
-      
-      // Create the full path for the upload directory
-      const fullPath = path.join(__dirname, '..', uploadDir);
-      
-      // Ensure the directory exists, if not, create it
       if (!fs.existsSync(fullPath)) {
         fs.mkdirSync(fullPath, { recursive: true });
       }
+    } catch (error) {
+      console.error('Error creating upload directory:', error);
+      throw new Error('Error creating upload directory');
+    }
 
-      // Save the file to the upload directory
+    // Save the file to the upload directory
+    try {
       const filePath = path.join(fullPath, originalname);
       fs.writeFileSync(filePath, file.buffer);
-
       return { message: 'File uploaded successfully' };
     } catch (error) {
-      console.error('Error uploading file:', error);
-      throw new Error('Error uploading file');
+      console.error('Error saving file:', error);
+      throw new Error('Error saving file');
     }
   }
 }
